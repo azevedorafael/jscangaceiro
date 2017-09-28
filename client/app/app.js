0dbaf1db7bd8f91a29c4768ad1907a2145e776a9
razevedo <razevedo@TF-1933>
2017-09-28 16:42:01 -0300

    Cria controller para Negociação

---
+++ client/app/app.js
@@ -0,0 +1,7 @@
+// cria a instância do controller
+let controller = new NegociacaoController();
+
+// associa o evento de submissão do formulário à chamada do método "adiciona"
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
