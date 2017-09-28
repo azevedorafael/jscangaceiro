cf4332bd8f97f132e3c418f5847ed7809994ddcb
Rafael da S.P. de Azevedo <rafaelsilva.fatec@gmail.com>
2017-09-28 01:02:18 -0300

    Adiciona imutabilidade ao construtor da classe Negociação

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,16 +1,27 @@
 class Negociacao {
 
-    constructor(data, quantidade, valor) {
+    constructor(_data, _quantidade, _valor) {
 
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+        // this._data = new Date(data.getTime());
+        // this._quantidade = quantidade;
+        // this._valor = valor;
+        // código abaixo diminui o número de instruções, se comparado acima
+
+        // Object.assign(this, {
+        //     _data: new Date(data.getTime()),
+        //     _quantidade,// =  _quantidade: _quantidade,
+        //     _valor
+        // });
+
+        //código acima mais legível abaixo
+        Object.assign(this, { _quantidade, _valor });
+        this._data = new Date(_data.getTime());
         Object.freeze(this);
     }
 
     get data() {
-
-        return this._data;
+        //programação defensiva contra mudanças indevidas na propriedade (mutabilidade)
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
@@ -27,7 +38,4 @@ class Negociacao {
 
         return this._quantidade * this._valor;
     }
-
-
-
 }
