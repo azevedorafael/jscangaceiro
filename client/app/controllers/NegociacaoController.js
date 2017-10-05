f6a703f6a98ddc53ba83a7867b4797d5bbc7d1a5
razevedo <razevedo@TF-1933>
2017-10-05 14:02:31 -0300

    Adiciona lista de Negociações

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,6 +6,7 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
@@ -17,14 +18,28 @@ class NegociacaoController {
         // let data = converter.paraData(this._inputData.value);
         // console.log(this._inputData.value);
 
-        let negociacao = new Negociacao(
+        this._negociacoes.adiciona(this._criaNegociacao());
+
+        //Limpando o formulário após criação da nova negociação
+        this._limpaFormulario();
+
+        // imprime a lista com o novo elemento
+        console.log(this._negociacoes.paraArray());
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+        // retorna uma instância de negociação
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-
-        let	diaMesAno	=	DateConverter.paraTexto(negociacao.data);
-        console.log(diaMesAno);
-
     }
 }
